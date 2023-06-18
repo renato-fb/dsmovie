110c29c4fadbd31c69290e13b08ba393868ede58
Renato Freitas <renatodefreitas_@hotmail.com>
2023-06-18 14:45:50 -0300

    Pagination layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 
-import { ReactComponent as GithubIcon } from 'assets/img/Vector.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
